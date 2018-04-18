a7d508df962b53affb70eda8633dd6b330883c2a
Arnaldo Assis <arnaldo_assisferreira@outlook.com>
2018-04-18 17:55:57 -0300

    modulo 5 - problema de cross origin

--- src/theme/Login.vue
+++ src/theme/Login.vue
@@ -1,45 +1,93 @@
 <template>
   <div class="content">
-	<h2>Login</h2>
-	<div class="field is-horizontal">
-		<div class="field-label is-normal">
-		  <label class="label">Username</label>
-		</div>
-		<div class="field-body">
-		  <div class="field">
-			<div class="control">
-			  <input class="input" type="text" 
-			  placeholder="Your username">
-			</div>
-		  </div>
-		</div>
-	</div>
-	<div class="field is-horizontal">
-		<div class="field-label is-normal">
-		  <label class="label">Password</label>
-		</div>
-		<div class="field-body">
-		  <div class="field">
-			<div class="control">
-			  <input class="input" type="password" 
-			  placeholder="Your password">
-			</div>
-		  </div>
-		</div>
-	</div>
-	<div class="field is-horizontal">
-		<div class="field-label">
-		  <!-- Left empty for spacing -->
-		</div>
-		<div class="field-body">
-		  <div class="field">
-			<div class="control">
-			  <button class="button is-primary">
-				Login
-			  </button>
-			</div>
-		  </div>
-		</div>
-	</div>
-</div>
+      <h2>Login</h2>
+
+      <div v-if="isLogado">
+          Usuario Logado
+          <button v-on:click="logout()" class="button is-primary">
+              LogOut
+          </button>
+      </div>
+      <div v-else>
+      <div class="field is-horizontal">
+        <div class="field-label is-normal">
+            <label  class="label">LO-GIN</label>
+        </div>
+        <div class="field-body">
+            <div class="field">
+                <div class="control">
+                    <input v-model="username" type="text" class="input" placeholder="SOU NOME">
+                </div>
+            </div>
+        </div>
+      </div>
+
+      <div class="field is-horizontal">
+          <div class="field-label is-normal">
+              <label class="label">SENHA</label>
+          </div>
+          <div class="field-body">
+              <div class="field">
+                  <div class="control">
+                      <input v-model="password" type="password" class="input" placeholder="SENHA">
+                  </div>
+              </div>
+          </div>
+      </div>
+
+      <div class="field is-horizontal">
+          <div class="field-label">
+            <!---->
+          </div>
+          <div class="field-body">
+              <div class="field">
+                  <div class="control">
+                      <button v-on:click="login()" class="button is-primary">LOGIN</button>
+                  </div>
+              </div>
+          </div>
+      </div>
+
+      </div>
+      
+      
+  </div>
 </template>
+<script>
+    import appService from '../app.service.js'
+
+    export default {
+      data () {
+        return {
+          username: '',
+          password: '',
+          isLogado: false
+        }
+      },
+      methods: {
+        login () {
+          appService.login({username: this.username, password: this.password})
+            .then((data) => {
+              window.localStorage.setItem('token', data.token)
+              window.localStorage.setItem('tokenExpiration', data.expiration)
+              this.isLogado = true
+              this.password = ''
+              this.username = ''
+            })
+            .catch(() => window.alert('Não pode logar!'))
+        },
+        logout () {
+          window.localStorage.setItem('token', null)
+          window.localStorage.setItem('tokenExpiration', null)
+          this.isLogado = false
+        }
+      },
+      created () {
+        let expiration = window.localStorage.getItem('tokenExpiration')
+        var unixTimeStamp = new Date().getTime() / 1000
+        if (expiration != null && parseInt(expiration) - unixTimeStamp > 0) {
+          this.isLogado = true
+        }
+      }
+}
+</script>
